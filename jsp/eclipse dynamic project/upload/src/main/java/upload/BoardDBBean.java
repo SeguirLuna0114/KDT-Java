5570e81ec1934abf129d9707fa0661e3e74020b7
SeguirLuna0114 <seguir.luna@gmail.com>
2023-10-13 16:59:47 +0900

    upload 1013 jsp project - reboard works

---
+++ jsp/eclipse dynamic project/upload/src/main/java/upload/BoardDBBean.java
@@ -0,0 +1,32 @@
+// DAO(Data Access Object)클래스
+
+package upload;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+public class BoardDBBean {
+
+	// 싱글톤 : 객체 생성을 1번만 수행한 후, 공유
+	private static BoardDBBean instance = new BoardDBBean();
+
+	// 정적 메소드 - 싱글톤으로 생성한 객체 반환
+	public static BoardDBBean getInstance() {
+		return instance;
+	}
+
+	// 커넥션 풀에서 커넥션을 구해오는 메소드(외부 접근 불가하게 private접근제어자 사용)
+	private Connection getConnection() throws Exception {
+		// JNDI(Java Naming and Directory Interface) 컨텍스트를 초기화
+		Context init = new InitialContext();
+		// JNDI에서 등록한 데이터 소스에 대한 참조를 가져옴
+		DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/orcl");
+		// 데이터 소스로부터 데이터베이스 연결(Connection)을 가져와 반환
+		return ds.getConnection();
+	}
+
+}
---
+++ jsp/문서/11.JSP를 이용한 자료실/cos-22.05.md
@@ -0,0 +1,22 @@
+# cos-22.05.zip
+
+
+### cos-22.05/doc/index.html
+
+cos라이브러리 API
+
+
+
+### cos-22.05/lib/cos.jar
+
+실행파일
+
+
+
+### cos-22.05\src\com\oreilly\servlet
+
+java파일
+
+
+
+
